2988ca04dfd1b92d1e983f23573ff857f2de0ad8
Sowmyasree Anumalisetti <sowmyasreea@infinite.com>
2025-07-07 15:59:37 +0530

    C Sharp

---
+++ C Sharp Assignment 7/C Sharp Assignment 7/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Assignment_7
+{
+    public class Class1
+    {
+
+        public string CalculateConcession(int age, double totalFare)
+        {
+            if (age <= 5)
+            {
+                return "Little Champs - Free Ticket";
+            }
+            else if (age > 60)
+            {
+                double concessionFare = totalFare * 0.7;
+                return $"Senior Citizen - Fare after concession: {concessionFare}";
+            }
+            else
+            {
+                return $"Ticket Booked for Fare: {totalFare}";
+            }
+        }
+    }
+}
