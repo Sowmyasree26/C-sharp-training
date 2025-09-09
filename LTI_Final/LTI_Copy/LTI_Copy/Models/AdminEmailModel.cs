b10bf24c830ac0e5aa301aa251dd3b3338f061ed
Sowmyasree Anumalisetti <sowmyasreea@infinite.com>
2025-09-09 15:22:45 +0530

    project

---
+++ LTI_Final/LTI_Copy/LTI_Copy/Models/AdminEmailModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LTI_Copy.Models
+{
+    public class AdminEmailModel
+    {
+        public string RecipientType { get; set; } // "User" or "Client"
+        public string EmailType { get; set; }     // e.g., "Welcome Mail"
+        public string ToEmail { get; set; }
+        public int RecipientID { get; set; }
+        public string CustomBody { get; set; }
+
+    }
+
+}
---
+++ LTI_Final/LTI_Copy/LTI_Copy/Models/DemoModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LTI_Copy.Models
+{
+    public class DemoModel
+    {
+    }
+}
